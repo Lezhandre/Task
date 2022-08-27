1bb3344ce8cdedbc7f2685b530ee7de64419c6b4
Michael <duzhenko.misha@mail.ru>
2022-08-27 07:19:18 +0300

    Simple implementation of PCAPReader

--- PCAP.cpp
+++ PCAP.cpp
@@ -1,1 +1,69 @@
 #include "PCAP.h"
+#include <fstream>
+
+void pcap_hdr_t::applyToAllFields(std::function<void(char*, size_t)> func) {
+    func((char*)&(this->magic_number), sizeof(this->magic_number));
+    func((char*)&(this->network), sizeof(this->network));
+    func((char*)&(this->sigfigs), sizeof(this->sigfigs));
+    func((char*)&(this->snaplen), sizeof(this->snaplen));
+    func((char*)&(this->thiszone), sizeof(this->thiszone));
+    func((char*)&(this->version_major), sizeof(this->version_major));
+    func((char*)&(this->version_minor), sizeof(this->version_minor));
+}
+
+void pcaprec_hdr_t::applyToAllFields(std::function<void(char*, size_t)> func) {
+    func((char*)&(this->incl_len), sizeof(this->incl_len));
+    func((char*)&(this->orig_len), sizeof(this->orig_len));
+    func((char*)&(this->ts_sec), sizeof(this->ts_sec));
+    func((char*)&(this->ts_usec), sizeof(this->ts_usec));
+}
+
+PCAPReader::PCAPReader(const std::string &fileName) : fileName(fileName) {
+    pcap_hdr_t pcap_header;
+    pcaprec_hdr_t pcap_pack_header;
+    std::ifstream fin(fileName, std::ios::binary);
+    fin.read((char*)&pcap_header, sizeof(pcap_hdr_t));
+    switch (pcap_header.magic_number)
+    {
+        case IDENTICAL_NUMBER:
+            fromRawView = [](char* data, size_t size){
+            };
+            break;
+        case SWAPPED_NUMBER:
+            fromRawView = [](char* data, size_t size){
+                for (size_t i = 0; i < size / 2; ++i)
+                    std::swap(data[i], data[size - 1 - i]);
+            };
+            break;
+        default:
+            throw std::runtime_error("Unknown format of file");
+    }
+    pcap_header.applyToAllFields(fromRawView);
+    fromRawView((char*)&pcap_header.magic_number, sizeof(pcap_header.magic_number));
+    if (fin.fail())
+        throw std::runtime_error("Global header is uncompleted");
+    while (!fin.eof()) {
+        fin.read((char*)&pcap_pack_header, sizeof(pcaprec_hdr_t));
+        pcap_pack_header.applyToAllFields(fromRawView);
+        if (fin.eof())
+            break;
+        if (fin.fail())
+            throw std::runtime_error("Record header is uncompleted");
+        if (pcap_pack_header.orig_len > pcap_header.snaplen)
+            throw std::runtime_error("Lengths in record header are incorrect");
+        if (pcap_pack_header.orig_len >= pcap_pack_header.incl_len)
+            packets.push_back({fin.tellg(), pcap_pack_header.orig_len});
+        else
+            packets.push_back({fin.tellg(), pcap_header.snaplen});
+        preparedPayloadSize += packets.back().payload_size;
+        fin.seekg(packets.back().payload_size, fin.cur);
+    }
+}
+
+uint64_t PCAPReader::packetsCount() const {
+    return packets.size();
+}
+
+uint64_t PCAPReader::payloadSize() const {
+    return preparedPayloadSize;
+}
--- PCAP.h
+++ PCAP.h
@@ -1,10 +1,42 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <functional>
 
+struct PCAPPacket {
+    std::streampos offset_in_document;
+    size_t payload_size;
+};
+
+#define IDENTICAL_NUMBER 0xa1b2c3d4
+#define SWAPPED_NUMBER 0xd4c3b2a1
+#define MODIFIED_NUMBER 0x0xa1b2cd34
+
+typedef struct pcap_hdr_s {
+    uint32_t magic_number;   
+    uint16_t version_major;  
+    uint16_t version_minor;  
+    int32_t  thiszone;       
+    uint32_t sigfigs;        
+    uint32_t snaplen;        
+    uint32_t network;        
+    void applyToAllFields(std::function<void(char*, size_t)>);
+} pcap_hdr_t;
+
+typedef struct pcaprec_hdr_s {
+    uint32_t ts_sec;         
+    uint32_t ts_usec;        
+    uint32_t incl_len;       
+    uint32_t orig_len;       
+    void applyToAllFields(std::function<void(char*, size_t)>);
+} pcaprec_hdr_t;
 
 class PCAPReader {
     const std::string fileName;
+    std::vector<PCAPPacket> packets;
+    size_t preparedPayloadSize = 0;
+    std::function<void(char*, size_t)> fromRawView;
 public:
     explicit PCAPReader(const std::string &fileName);
 
